465b82487cbf6d55a292c3565653119ba0fcb879
Abdullah Alger <abdullahalger@me.com>
2014-10-30 23:56:01 +0200

    Still trying to figure out ngCordova

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,16 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('everVoice', ['ionic'])
+angular.module('everVoice', ['ionic', 'ngCordova'])
 
-.controller('RecordVoice', ['$scope', '$ionicPopup', 'VoiceService', function($scope, $ionicPopup, VoiceService){
+.controller('RecordVoice', ['$scope', '$ionicPopup', 'VoiceService', function($scope, $ionicPopup, $cordovaMedia, VoiceService){
+
 
 // Evernote Popup - Was Formerly another Controller
 $scope.showConfirm = function() {
    var confirmPopup = $ionicPopup.confirm({
      title: 'Save Recording',
-     template: 'Do you want to save your voice message?' });
+     template: 'Save to Evernote?' });
    confirmPopup.then(function(res) {
      if(res) {
        console.log('You are sure');
@@ -30,38 +31,18 @@ $scope.showConfirm = function() {
 
 }])
 
+// File Cordova to send to att ???
 
+.factory('VoiceService',['$rootScope', function($rootScope){
 
-.service('VoiceService', ['$scope', function($scope){
+  var currentRecording = null;
 
-// Record Function
-  $scope.record = function() {
-    var src = "??";
-    var mediaRec = new Media(src,
-    // Success message  
-      function() {
-        console.log("record success");
-      }, 
-    // Error message
-      function(err) {
-        console.log("record error " + err.code);
-      });
-    // Starts recording
-    mediaRec.startRecord();
-    // Sets a timeout for the recording if not stopped
-    setTimeout(function() {
-      mediaRec.stopRecord();
-      }, 60000);
-    };
+  return {
+  onTimeUpdate: function(callback) {
+      return $rootScope.$on('recording:timeupdate', callback);
+    }
+  };
 
-// Stop Function
-  $scope.stop = function() {
-    mediaRec.stopRecord();
-  }; 
-
-// Record Duration 
-  
-  
 
 }])
 
