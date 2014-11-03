41e19b4ef7a566aa1bd8e0e9cf06c054617f0d4b
Abdullah Alger <abdullahalger@me.com>
2014-11-03 22:33:36 +0200

    Starting Evernote Directive

--- www/js/app.js
+++ www/js/app.js
@@ -18,27 +18,21 @@ var evernote = angular.module('everVoice', ['ionic', 'ngCordova', 'ngCordovaMock
   });
 });
 
-evernote.controller('Capture', function($scope, $cordovaCamera){
-     $scope.takePicture = function() {
-        var options = { 
-            quality : 40, 
-            destinationType : Camera.DestinationType.FILE_URL, 
-            sourceType : Camera.PictureSourceType.CAMERA, 
-            allowEdit : true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
-            popoverOptions: CameraPopoverOptions,
-            saveToPhotoAlbum: false
-        };
- 
-        $cordovaCamera.getPicture(options).then(function(imageData) {
-            $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-            // An error occured. Show a message to the user
-        });
-    }
+evernote.controller('RecordCtrl', function($scope, $cordovaMedia) {
+
+  $scope.captureAudio = function() {
+    var options = { duration: 10 };
+
+    $cordovaCapture.captureAudio(options).then(function(audioData) {
+      // Success! Audio data is here
+    }, function(err) {
+      // An error occured. Show a message to the user
+    });
+  }
+
+
 });
+
 
 // Evernote Popup Controller
 
@@ -58,21 +52,72 @@ evernote.controller('PopupCtrl', function($scope, $ionicPopup){
 });
 // File Cordova to send to att ???
 
+evernote.directive('sendATT', function($scope) {
+// send to ATT
+
+
+
+});
+
+evernote.directive('createEvernote', function($scope) {
+// Evernote creates a new note
+
+  var Evernote = require('evernote').Evernote;
+
+  module.exports = function(
+    token,
+    noteTitle,
+    noteBody,
+    isReminder,
+    callback
+  ) {
+
+    var client = new Evernote.Client({token: token, sandbox: false});
+    var noteStore = client.getNoteStore();
+
+    console.log(noteStore);
+
+    var nBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+    nBody += "<!DOCTYPE en-note SYSTEM \"http://xml.evernote.com/pub/enml2.dtd\">";
+    nBody += "<en-note>" + noteBody + "</en-note>";
+
+    // Create note object
+    var ourNote = new Evernote.Note();
+    ourNote.title = noteTitle;
+    ourNote.content = nBody;
+    ourNote.attributes = new Evernote.NoteAttributes();
+    if (isReminder) {
+      ourNote.attributes.reminderOrder = 0;
+    }
+
+    // Attempt to create note in Evernote account
+    noteStore.createNote(ourNote, function(err, note) {
+      callback(err, note);
+    });
+  };
+
+
+
+
+});
+
+
+
 
 evernote.filter('timecode', function(){
    return function(seconds) {
      seconds = Number.parseFloat(seconds);
- 
+
      // Returned when No Time is Provided
      if (Number.isNaN(seconds)) {
        return '-:--';
      }
- 
+
      // Returns a Whole Number
      var wholeSeconds = Math.floor(seconds);
      var minutes = Math.floor(wholeSeconds / 60);
      var remainingSeconds = wholeSeconds % 60;
- 
+
      var output = minutes + ':';
      // zero pad seconds, so 9 seconds should be :09
      if (remainingSeconds < 10) {
